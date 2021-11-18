b0198f45cbb7588993155fded44a337a495f5e0c
amrdevac <amrullahdev@yahoo.com>
2021-11-18 12:33:19 +0700

    modul konsumer key untuk mengelola data client id dan secret yang nantinya dipakai untuk kebutuhkan authorize open api yang di sediakan

---
+++ app/Http/Controllers/API/KonsumerKeyController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Services\KonsumerKeyService;
+use App\Services\responseService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ResetKonsumerKeySecretRequest;
+use App\Http\Requests\StoreKonsumerKeyRequest;
+
+class KonsumerKeyController extends Controller
+{
+
+    private KonsumerKeyService $konsumerkeysService;
+    private ResponseService $responseService;
+
+    public function __construct(
+        KonsumerKeyService $konsumerkeysService,
+        ResponseService $responseService
+    ) {
+        $this->konsumerkeysService = $konsumerkeysService;
+        $this->responseService = $responseService;
+    }
+
+    public function index()
+    {
+        $in_konsumer_key = $this->konsumerkeysService->mendapatkanSeluruhDataPaginate($this->paginate);
+        return compact("in_konsumer_key");
+    }
+
+    public function search(Request $request)
+    {
+        $in_konsumer_key = $this->konsumerkeysService->mencariDataBerdasarkanKostum("client_id", $request->cari, $this->paginate);
+        return compact("in_konsumer_key");
+    }
+
+    public function create()
+    {
+        return null;
+    }
+
+    public function edit($id)
+    {
+        $in_konsumer_key = $this->konsumerkeysService->mendapatkanSatuData($id);
+        return compact("in_konsumer_key");
+    }
+
+    public function store(StoreKonsumerKeyRequest $request)
+    {
+        return $this->responseService->menyimpanData($this->konsumerkeysService->menyimpanData($request));
+    }
+
+    public function show($id)
+    {
+        $in_konsumer_key = $this->konsumerkeysService->mendapatkanSatuData($id);
+        return compact("in_konsumer_key");
+    }
+
+    public function update(Request $request, $id)
+    {
+        return $this->responseService->updateData($this->konsumerkeysService->memperbaruiData($request, $id));
+    }
+
+    public function destroy($id)
+    {
+        $this->konsumerkeysService->menghapusData($id);
+        return $this->responseService->menghapusData($id);
+    }
+
+    public function resetSecret(ResetKonsumerKeySecretRequest $request, $id)
+    {
+        return $this->responseService->updateData($this->konsumerkeysService->meresetDataClientSecret($request, $id));
+    }
+}
