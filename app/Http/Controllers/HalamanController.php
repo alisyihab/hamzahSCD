dd263f299c2d7aca7dc1e8edcb048d834a7a059a
amrdevac <amrullahdev@yahoo.com>
2021-11-18 12:32:09 +0700

    menghapus independi yang tidak terpakai

--- app/Http/Controllers/HalamanController.php
+++ app/Http/Controllers/HalamanController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Actions\GetRolePermission;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HalamanController extends Controller
