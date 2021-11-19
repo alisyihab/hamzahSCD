96667edcfe883c8136ccc81b4a26493c97252a70
amrdevac <amrullahdev@yahoo.com>
2021-11-19 17:51:15 +0700

    fixing bug pencarian + paginate

--- resources/vue/auth/auditTrail/index.vue
+++ resources/vue/auth/auditTrail/index.vue
@@ -26,7 +26,7 @@
             </div>
             <section v-if="isPencarian">
                Hasil Dari : {{cari_data}}
-               <div class="text-blue cp" @click="load()">Reset</div>
+               <div class="text-blue cp" @click="resetPencarian()">Reset</div>
             </section>
             <div class="py-2 table-responsive">
                <table class="table table-hover">
@@ -65,25 +65,6 @@
                      <td
                         class="px-3 align-middle text-center small"
                      >{{ $date(audit_trail.created_at).format('dddd, DD/MMM/YY HH:mm') }}</td>
-                     <td class="px-3 align-middle text-center" width="25" v-if="isEditableData">
-                        <div class="btn-group">
-                           <button class="btn btn-sm" type="button" data-toggle="dropdown">
-                              <i class="fa fa-ellipsis-v"></i>
-                           </button>
-                           <div class="dropdown-menu dropdown-menu-right">
-                              <router-link
-                                 v-if="canDoUpdate"
-                                 :to="'audit-trail/create/'+audit_trail.id"
-                                 class="dropdown-item"
-                              >Edit</router-link>
-                              <div
-                                 v-if="canDoDestroy"
-                                 class="dropdown-item cp"
-                                 @click="hapus(audit_trail.id)"
-                              >Delete</div>
-                           </div>
-                        </div>
-                     </td>
                   </tr>
                </table>
                <div class="container">
@@ -107,43 +88,64 @@ export default {
          isEditableData: false,
          in_audit_trail: {},
          cari_data: "",
-         canDoStore: false,
-         canDoUpdate: false,
-         canDoDestroy: false,
-         grup_url: "audit-trail"
+         grup_url: "",
+         queryUrlIfExist: ""
       };
    },
+
    mounted() {
-      this.verify_permission();
+      this.grup_url = this.$router.currentRoute.name.split(".")[0];
+      this.queryUrlIfExist = this.$router.currentRoute.query;
       this.load();
    },
+
    methods: {
+      checkIsPencarianTrue() {
+         if (this.$router.currentRoute.query.cari) {
+            this.cari_data = this.$router.currentRoute.query.cari;
+            return true;
+         }
+         return false;
+      },
+
+      resetQueryIfExist() {
+         this.$router.push(this.grup_url);
+         this.queryUrlIfExist = [];
+      },
+
+      resetPencarian() {
+         this.cari_data = null;
+         this.resetQueryIfExist();
+         this.load();
+      },
+
+      updateRouteUrl(data) {
+         this.$router.push({
+            path: this.$router.currentRoute.fullPath,
+            query: data
+         });
+         this.queryUrlIfExist = this.$router.currentRoute.query;
+      },
+
       loadPaginate(page = 1) {
-         this.$router.push(this.$router.currentRoute.path + "?page=" + page);
-         axios.get(this.$api_audit_trail + "?page=" + page).then(respon => {
-            this.in_audit_trail = respon.data.in_audit_trail;
-         });
+         this.updateRouteUrl({ page: page });
+         axios
+            .get(this.$api_audit_trail, {
+               params: this.queryUrlIfExist
+            })
+            .then(respon => {
+               this.in_audit_trail = respon.data.in_audit_trail;
+            });
       },
-      verify_permission() {
-         window.amr_data_permission_users.forEach(permission => {
-            if (permission.grup == this.grup_url) {
-               let data_permission = permission.url.split(".")[1];
-               if (data_permission == "store") {
-                  this.canDoStore = true;
-               }
-               if (data_permission == "update") {
-                  this.canDoUpdate = true;
-               }
-               if (data_permission == "destroy") {
-                  this.canDoDestroy = true;
-               }
-            }
-         });
-      },
+
       pencarian() {
+         this.resetQueryIfExist();
+         this.updateRouteUrl({ cari: this.cari_data });
          this.$Progress.start();
          axios
-            .get("/api/audit-trail/pencarian?cari=" + this.cari_data)
+            .get(this.$api_audit_trail, {
+               params: this.queryUrlIfExist
+            })
             .then(respon => {
                this.isPencarian = true;
                this.$Progress.finish();
@@ -154,11 +156,14 @@ export default {
                this.$error.catch(e);
             });
       },
+
       load() {
-         this.isPencarian = false;
+         this.isPencarian = this.checkIsPencarianTrue();
          this.$Progress.start();
          axios
-            .get(this.$api_audit_trail)
+            .get(this.$api_audit_trail, {
+               params: this.queryUrlIfExist
+            })
             .then(respon => {
                this.$Progress.finish();
                this.in_audit_trail = respon.data.in_audit_trail;
@@ -167,22 +172,6 @@ export default {
                this.$Progress.fail();
                this.$error.catch(e);
             });
-      },
-      hapus(data_kode) {
-         konfirmasiHapus.fire().then(result => {
-            if (result.isConfirmed) {
-               this.$toast.df102();
-               axios
-                  .delete("/api/audit-trail/" + data_kode)
-                  .then(() => {
-                     this.$toast.df200();
-                     this.load();
-                  })
-                  .catch(e => {
-                     this.$error.catch(e);
-                  });
-            }
-         });
       }
    }
 };
