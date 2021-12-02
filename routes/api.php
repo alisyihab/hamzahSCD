25cce19d4db85361aa3a6eebca47db545b89c359
amrdevac <amrullahdev@yahoo.com>
2021-12-02 15:23:13 +0700

    menambahkan api terbaru

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AlamatController;
 use App\Http\Controllers\API\JenisMemberController;
 use App\Http\Controllers\API\KostumerMemberController;
 
 Route::middleware(['auth:api', "akses"])->group(function () {
     Route::resources(["jenis-member" => JenisMemberController::class]);
+    Route::resources(["kostumer-member" => KostumerMemberController::class]);
+
+    Route::prefix("alamat")->name("alamat.")->group(function(){
+        Route::post("/provinsi", [AlamatController::class,"provinsi"])->name("provinsi");
+        Route::post("/wilayah", [AlamatController::class,"wilayah"])->name("wilayah");
+        Route::post("/kecamatan", [AlamatController::class,"kecamatan"])->name("kecamatan");
+        Route::post("/kelurahan", [AlamatController::class,"kelurahan"])->name("kelurahan");
+        Route::post("/kodepos", [AlamatController::class,"kode_pos"])->name("kode_pos");
+        Route::get("/negara", [AlamatController::class,"negara"])->name("negara");
+    });
 });
-
-
-Route::resources(["kostumer-member" => KostumerMemberController::class]);
-
-
-
-
 
 
 // JANGAN LUPA amr:getAllRoutes setiap nambahin route
