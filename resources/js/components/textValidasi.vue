0fb6dbb4142d5458e3178639f545a3bd354ad4b9
amrdevac <amrullahdev@yahoo.com>
2021-12-02 15:21:16 +0700

    membuat komponen validasi text error

---
+++ resources/js/components/textValidasi.vue
@@ -0,0 +1,13 @@
+<template>
+   <div class="text-red small">{{msg}}</div>
+</template>
+<script>
+export default {
+   props: ["msg"],
+   data() {
+      return {};
+   },
+   mounted() {},
+   methods: {}
+};
+</script>
